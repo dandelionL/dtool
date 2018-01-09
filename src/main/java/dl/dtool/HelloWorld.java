63d40fabea0b0724e803063238c48c5a3ca51494
dandelionL <longxinyin88@163.com>
2018-01-09 18:04:33 +0800

    HelloWorld类提交

---
+++ src/main/java/dl/dtool/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright for DL
+ */
+package dl.dtool;
+
+/**
+ * @info 
+ * @author DandelionL.Yin
+ * @version $Id: HelloWorld.java, v 0.1 2018年1月9日 下午5:45:19 DandelionL.Yin Exp $
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is just \"Hi\"");
+	}
+
+}
