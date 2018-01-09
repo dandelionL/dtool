48eeb27263b1faefdc865f1df142df4aefa892ce
dandelionL <longxinyin88@163.com>
2018-01-09 17:04:27 +0800

    share project

---
+++ src/main/java/dl/dtool/App.java
@@ -0,0 +1,13 @@
+package dl.dtool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
